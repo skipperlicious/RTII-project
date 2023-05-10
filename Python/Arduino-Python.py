98af61f75d84d6de147eeb1f1a6a5a8eb90a0735
Mikkel Lauridsen <xeroxa42@gmail.com>
2023-05-10 13:24:53 +0200

    Update Arduino-Python.py

--- Python/Arduino-Python.py
+++ Python/Arduino-Python.py
@@ -1,13 +1,16 @@
-#Dowload their names through pip
+#Imported libraries
 import pyautogui
 import keyboard
 import time
-#Download pyserial through pip
+import cv2
+import dlib
+import math
 import serial
 import serial.tools.list_ports
-import dataclasses
+import threading
 
-programRunning = True
+arduinoRunning = True
+imgprocessingRunning = True
 
 screenSizeX = 1920
 screenSizeY = 1080
@@ -21,6 +24,8 @@ positiveSideY = screenSizeY/2
 interval = 3
 
 start_time = time.time()
+
+BLINK_RATIO_THRESHOLD = 6
 
 # Get a list of active ports
 ports = serial.tools.list_ports.comports()
@@ -36,74 +41,226 @@ pyautogui.moveTo(screenSizeX/2, screenSizeY/2)  # moves mouse to X of 100, Y of 
 # Set the serial port based on the user's choice
 ser = serial.Serial(ports[choice-1].device, 38400)
 
-
-
 rolling_array_x = []
 rolling_array_y = []
-rolling_array_input1 = []
-rolling_array_input2 = []
-rolling_array_input3 = []
 
 pyautogui.FAILSAFE = False
 pyautogui.PAUSE = False
 
-counter = 0
-maxCount = 30
+def midpoint(point1 ,point2):
+    return (point1.x + point2.x)/2,(point1.y + point2.y)/2
 
+def euclidean_distance(point1 , point2):
+    return math.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
+
+def get_blink_ratio(eye_points, facial_landmarks):
     
-while programRunning:
-    if ser.in_waiting > 0:  # Check if there is new data available on the serial port
-        data = ser.readline().decode().strip()  # Read data from serial port
-        print(data)
-        input1, input2, input3, input4 = data.split(",")
-        print(input1, input2, input3, input4)
-        abs(float(input3))
-        newPercentX = float(input3) * 0.0001
-        y = "{:.3f}".format(newPercentX)
+    #loading all the required points
+    corner_left  = (facial_landmarks.part(eye_points[0]).x, 
+                    facial_landmarks.part(eye_points[0]).y)
+    corner_right = (facial_landmarks.part(eye_points[3]).x, 
+                    facial_landmarks.part(eye_points[3]).y)
+    
+    center_top    = midpoint(facial_landmarks.part(eye_points[1]), 
+                             facial_landmarks.part(eye_points[2]))
+    center_bottom = midpoint(facial_landmarks.part(eye_points[5]), 
+                             facial_landmarks.part(eye_points[4]))
 
-        abs(float(input4))
-        newPercentY = float(input4) * 0.0001
-        y2 = "{:.3f}".format(newPercentY)
-        
-        mousePosX = positiveSideX * float(y)
-        #print(newPercentX)
-        #print(mousePosX)
+    #calculating distance
+    horizontal_length = euclidean_distance(corner_left,corner_right)
+    vertical_length = euclidean_distance(center_top,center_bottom)
 
-        mousePosY = positiveSideY * float(y2)
-        #print(newPercentY)
-        #print(mousePosY)
-        
-        #print('END')
-        #print('\n')
-        
-        #pyautogui.moveTo(screenSizeX, screenSizeY/2, _pause = False)  # moves mouse to X of 100, Y of 200.
-        pyautogui.moveRel(mousePosX, 0, _pause = False)  # moves mouse to X of 100, Y of 200.
-        pyautogui.moveRel(0, mousePosY, _pause = False)  # moves mouse to X of 100, Y of 200.
+    ratio = horizontal_length / vertical_length
 
-        
-        
-        
-        
+    return ratio
 
-        lock_time = 0  # Initialize the lock time to 0
+def add_value_to_rolling_array_x(value):
+    rolling_array_x.append(value)
+    if len(rolling_array_x) > 5:
+        rolling_array_x.pop(0)
 
-        if int(input1) == 1:  # If the input is LEFT, simulate a left mouse click
-            if time.time() - lock_time > interval:  # Check if enough time has elapsed since the last output
-                pyautogui.click(button='left')
-                lock_time = time.time()  # Update the lock time to the current time
-        if int(input2) == 2:  # If the input is RIGHT, simulate a right mouse click
-            if time.time() - lock_time > interval:  # Check if enough time has elapsed since the last output
-                pyautogui.click(button='right')
-                lock_time = time.time()  # Update the lock time to the current time
-        if int(input1) == 3:  # If the input is RIGHT, simulate a double click
-            if time.time() - lock_time > interval:  # Check if enough time has elapsed since the last output
-                pyautogui.doubleClick()
-                lock_time = time.time()  # Update the lock time to the current time
-                delta_time = time.time() - start_time
-                if(delta_time <= interval):
-                    start_time = time.time()
+def get_rolling_array_average_x():
+    if len(rolling_array_x) == 0:
+        return None
+    else:
+        return sum(rolling_array_x) / len(rolling_array_x)
+
+def add_value_to_rolling_array_y(value):
+    rolling_array_y.append(value)
+    if len(rolling_array_y) > 5:
+        rolling_array_y.pop(0)
+
+def get_rolling_array_average_y():
+    if len(rolling_array_y) == 0:
+        return None
+    else:
+        return sum(rolling_array_y) / len(rolling_array_y)
+
+#livestream from the webcam
+cap = cv2.VideoCapture(0)
+
+'''in case of a video
+cap = cv2.VideoCapture("__path_of_the_video__")'''
+
+#name of the display window in openCV
+cv2.namedWindow('BlinkDetector')
+
+#Init dlib front face detector
+detector = dlib.get_frontal_face_detector()
+
+#init dlib predictor
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+#these landmarks are based on the image above 
+left_eye_landmarks  = [36, 37, 38, 39, 40, 41]
+right_eye_landmarks = [42, 43, 44, 45, 46, 47]
+
+def arduino_loop():
+    while arduinoRunning:
+        if ser.in_waiting > 0:  # Check if there is new data available on the serial port
+            data = ser.readline().decode().strip()  # Read data from serial port
+            input1, input2, input3, input4 = data.split(",")
+            print(input1, input2, input3, input4)
+
+            abs(float(input3))
+            newPercentX = float(input3) * 0.0001
+            y = "{:.3f}".format(newPercentX)
+
+            abs(float(input4))
+            newPercentY = float(input4) * 0.0001
+            y2 = "{:.3f}".format(newPercentY)
+            
+            mousePosX = positiveSideX * float(y)
+            #print(newPercentX)
+            #print(mousePosX)
+
+            mousePosY = positiveSideY * float(y2)
+            #print(newPercentY)
+            #print(mousePosY)
+            
+            #print('END')
+            #print('\n')
+            
+            #pyautogui.moveTo(screenSizeX, screenSizeY/2, _pause = False)  # moves mouse to X of 100, Y of 200.
+            pyautogui.moveRel(mousePosX, 0, _pause = False)  # moves mouse to X of 100, Y of 200.
+            pyautogui.moveRel(0, mousePosY, _pause = False)  # moves mouse to X of 100, Y of 200.
+
+            
+            
+            add_value_to_rolling_array_x(mousePosX)
+
+            average_x = get_rolling_array_average_x()
+            #print(average_x)
+
+            add_value_to_rolling_array_y(mousePosY)
+
+            average_y = get_rolling_array_average_y()
+            #print(average_y)
+
+            
+
+            
+            
+
+            '''
+            lock_time = 0  # Initialize the lock time to 0
+
+            if input1 != prev_input1:
+                if input1 == 1:
+                    if time.time() - lock_time > 0.75:
+                        pyautogui.click(button='left')
+                        lock_time = time.time()
+                elif input1 == 3:
+                    if time.time() - lock_time > 0.75:
+                        pyautogui.doubleClick()
+                        lock_time = time.time()
+
+            if input2 != prev_input2:
+                if input2 == 2:
+                    if time.time() - lock_time > 0.75:
+                        pyautogui.click(button='right')
+                        lock_time = time.time()
+
+            if input3 != prev_input3:
+                # Handle input 3 here
+                pass
+
+            prev_input1 = input1
+            prev_input2 = input2
+            prev_input3 = input3
+
+            '''
+        if keyboard.is_pressed("ctrl"):
+            ser.close
+            cap.release()
+            cv2.destroyAllWindows()
+            print("Terminating...")
+            break
+
+def blink_detector_loop():
+    while True:
+        #capturing frame
+        retval, frame = cap.read()
         
-    if keyboard.is_pressed("ctrl"):
-        ser.close
-        print("Terminating...")
-        break
+        #exit the application if frame not found
+        if not retval:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break 
+
+        #-----Step 2: converting image to grayscale-----
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        #-----Step 3: Face detection with dlib-----
+        #detecting faces in the frame 
+        faces,_,_ = detector.run(image = frame, upsample_num_times = 0, adjust_threshold = 0.0)
+
+        for face in faces:
+        
+            landmarks = predictor(frame, face)
+
+            #-----Step 5: Calculating blink ratio for one eye-----
+            right_eye_ratio  = get_blink_ratio(left_eye_landmarks, landmarks)
+            left_eye_ratio = get_blink_ratio(right_eye_landmarks, landmarks)
+            blink_ratio     = (left_eye_ratio + right_eye_ratio) / 2
+
+            #print(left_eye_ratio)
+            if left_eye_ratio > BLINK_RATIO_THRESHOLD:
+                #Blink detected! Do Something!
+                cv2.putText(frame,"LEFT BLINKING",(10,50), cv2.FONT_HERSHEY_SIMPLEX,2,(255,255,255),2,cv2.LINE_AA)
+                print("YOU BLINKED LEFT")
+
+            if right_eye_ratio > BLINK_RATIO_THRESHOLD:
+                #Blink detected! Do Something!
+                cv2.putText(frame,"RIGHT BLINKING",(10,100), cv2.FONT_HERSHEY_SIMPLEX,2,(255,255,255),2,cv2.LINE_AA)
+                print("YOU BLINKED RIGHT")
+            
+            #if blink_ratio > BLINK_RATIO_THRESHOLD:
+                #Blink detected! Do Something!
+            #   cv2.putText(frame,"BLINKING",(10,50), cv2.FONT_HERSHEY_SIMPLEX,
+            #  2,(255,255,255),2,cv2.LINE_AA)
+
+        #cv2.imshow('BlinkDetector', frame)
+
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+    
+        if keyboard.is_pressed("ctrl"):
+            ser.close
+            cap.release()
+            cv2.destroyAllWindows()
+            print("Terminating...")
+            break
+
+# Create and start threads
+blink_detector_thread = threading.Thread(target=blink_detector_loop)
+blink_detector_thread.start()
+
+time.sleep(3)
+
+# Create and start threads
+arduino_thread = threading.Thread(target=arduino_loop)
+arduino_thread.start()
+
+# Wait for threads to finish
+arduino_thread.join()
+blink_detector_thread.join()
